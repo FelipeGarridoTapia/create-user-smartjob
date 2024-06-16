527350258917d2d35b8bd4567922d5001cbcd863
fgarrido <fa.garridotapia@gmail.com>
2024-06-16 12:01:17 -0400

    optimized imports

--- src/main/java/com/smartjob/createuser/dtos/request/UserDto.java
+++ src/main/java/com/smartjob/createuser/dtos/request/UserDto.java
@@ -1,6 +1,5 @@
 package com.smartjob.createuser.dtos.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
